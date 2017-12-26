894e9d4892d076204dbacbfd5e242ef196ab2cf2
heliazkh <heliazolfkhani85@gmail.com>
2017-12-26 12:33:18 +0330

    add firstname and lastname

--- action_page.php
+++ action_page.php
@@ -1,4 +1,12 @@
 <?php
 
 echo "Hi ".$_POST['login']['firstname'];
+
+
+foreach ($_POST['login'] as $key => $value) {
+	echo "<p><b>$key</b></p>";
+	echo "<ul>";
+	echo "<li>".$value."</li>";
+	echo "</ul>";
+}
 ?>
